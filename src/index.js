0813122b68b140e5a44da55fc7874b2a5df49e2f
DanielTolentino <danieltolentino@protonmail.com>
2024-09-23 19:34:02 -0300

    Add title and favicon to index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
+      <title>Clima App</title>
+      <link rel="icon" href="☀️" />
+    </>
     <App />
   </React.StrictMode>
 );
