74bd727ff9510861bbdc0097937380481dcb5ab2
DanielTolentino <danieltolentino@protonmail.com>
2024-09-23 19:36:42 -0300

    Refactor index.html and index.js for Clima App branding

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <>
-      <title>Clima App</title>
-      <link rel="icon" href="☀️" />
-    </>
     <App />
   </React.StrictMode>
 );
